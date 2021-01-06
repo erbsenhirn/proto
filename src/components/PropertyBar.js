d25ee397a2cb2d05901467d62bd43792ac48bd8e
erbsenhirn <erbsenhirn@web.de>
2021-01-06 18:13:44 +0100

    Remove loop in propertybar

--- src/components/PropertyBar.js
+++ src/components/PropertyBar.js
@@ -19,27 +19,23 @@ const PropertyBar = (props) => {
       </Box>
     )
   } else {
-    for (const [id, element] of Object.entries(elements)) {
-      if (id === selection) {
-        return (
-          <Box>
-            <Property verbose='Text' slug='text' value={element.text} />
-            <Property verbose='Text Size' slug='fontSize' value={element.fontSize} />
-            <Property verbose='Text Align' slug='textAlign' value={element.textAlign} />
-            <Property verbose='Text Color' slug='color' value={element.color} />
-            <Property verbose='Background Color' slug='backgroundColor' value={element.backgroundColor} />
-            <Property verbose='Border' slug='border' value={element.border} />
-            <Property verbose='Border Radius' slug='borderRadius' value={element.borderRadius} />
-            <Property verbose='Width' slug='width' value={element.width} />
-            <Property verbose='Height' slug='height' value={element.height} />
-            <Property verbose='X' slug='left' value={element.left} />
-            <Property verbose='Y' slug='top' value={element.top} />
-            <Property verbose='Margin' slug='margin' value={element.margin} />
-            <Property verbose='Padding' slug='padding' value={element.padding} />
-          </Box>
-        )
-      }
-    }
+    return (
+      <Box>
+        <Property verbose='Text' slug='text' value={elements[selection].text} />
+        <Property verbose='Text Size' slug='fontSize' value={elements[selection].fontSize} />
+        <Property verbose='Text Align' slug='textAlign' value={elements[selection].textAlign} />
+        <Property verbose='Text Color' slug='color' value={elements[selection].color} />
+        <Property verbose='Background Color' slug='backgroundColor' value={elements[selection].backgroundColor} />
+        <Property verbose='Border' slug='border' value={elements[selection].border} />
+        <Property verbose='Border Radius' slug='borderRadius' value={elements[selection].borderRadius} />
+        <Property verbose='Width' slug='width' value={elements[selection].width} />
+        <Property verbose='Height' slug='height' value={elements[selection].height} />
+        <Property verbose='X' slug='left' value={elements[selection].left} />
+        <Property verbose='Y' slug='top' value={elements[selection].top} />
+        <Property verbose='Margin' slug='margin' value={elements[selection].margin} />
+        <Property verbose='Padding' slug='padding' value={elements[selection].padding} />
+      </Box>
+    )
   }
 }
 
