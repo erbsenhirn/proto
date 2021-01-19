0ba2cdba1902079ebfa4188e038900a55f1251f4
erbsenhirn <erbsenhirn@web.de>
2021-01-19 14:02:52 +0100

    Add Image input

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -2,20 +2,52 @@ import React from 'react'
 import { Button, Box } from '@material-ui/core'
 import { Folder, TextFields, Image } from '@material-ui/icons'
 
-import { addElement } from '../api'
+import { addTextElement } from '../api'
 
 const ToolBar = () => {
+  const hiddenFileInput = React.useRef(null)
+  const handleUploadClick = event => {
+    hiddenFileInput.current.click()
+  }
+  const handleUpload = event => {
+    const fileUploaded = event.target.files[0]
+    console.log(fileUploaded)
+  }
+
   return (
     <Box>
-      <Button onClick={ () => addElement('Text') } variant="outlined" color="primary" startIcon={<TextFields />} >
+      <Button
+        onClick={ () => addTextElement('Text') }
+        variant='outlined'
+        color='primary'
+        startIcon={<TextFields />}
+      >
         Add Text
       </Button>
-      <Button variant="outlined" color="primary" startIcon={<Image />} disabled >
+      <Button
+        onClick={ handleUploadClick }
+        variant='outlined'
+        color='primary'
+        startIcon={<Image />}
+        type='file'
+      >
         Add Image
       </Button>
-      <Button variant="outlined" color="primary" startIcon={<Folder />} disabled >
+      <Button
+        variant='outlined'
+        color='primary'
+        startIcon={<Folder />}
+        disabled
+      >
         Add Container
       </Button>
+      <input
+        type='file'
+        ref={hiddenFileInput}
+        onChange={handleUpload}
+        style={{ display: 'none' }}
+        accept='image/*'
+      />
     </Box>
   )
 }
